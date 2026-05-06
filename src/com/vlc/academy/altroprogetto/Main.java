8593fb4e9bc39b34319647bce77ec46f16da8a32
stefano-pirastru <stefano.pirastru2001@gmail.com>
2026-05-06 11:54:18 +0200

    Nuova cartella e refactor packages

---
+++ src/com/vlc/academy/altroprogetto/Main.java
@@ -0,0 +1,7 @@
+package com.vlc.academy.altroprogetto;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
--- src/com/vlc/academy/ristorante/Main.java
+++ src/com/vlc/academy/ristorante/Main.java
@@ -1,4 +1,4 @@
-package com.vlc.academy;
+package com.vlc.academy.ristorante;
 
 public class Main{
     public static void main(String[] args){
